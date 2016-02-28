f0f84db685d0154d4df700f0f5a9d0984f5241a7
kamster <kamster94@gmail.com>
2016-02-28 20:51:54 +0100

    Signed-off-by: kamster <kamster94@gmail.com>

---
+++ strider/src/dbConnection/dbAccess.java
@@ -0,0 +1,16 @@
+package dbConnection;
+
+import java.util.List;
+
+public class dbAccess {
+	
+	private String connectionString;
+	
+	public Object pullFromDb(String sql, List<Object> params){
+		return void.class;
+	}
+	
+	public void pushToDb(String sql, List<Object> params){
+		
+	}
+}
