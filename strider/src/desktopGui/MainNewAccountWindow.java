5e748df070a4ecd6c818d34900fcb7be63448ea9
Chappi <chappi555@gmail.com>
2016-03-03 17:05:40 +0100

    test2

---
+++ strider/src/desktopGui/MainNewAccountWindow.java
@@ -0,0 +1,63 @@
+package desktopGui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class MainNewAccountWindow 
+{
+	    @FXML
+	    private Button buttoncancel;
+
+	    @FXML
+	    private Button buttoncreate;
+
+	    @FXML
+	    private Label labelusername;
+
+	    @FXML
+	    private TextField textfieldusername;
+
+	    @FXML
+	    private TextField textfieldemail;
+
+	    @FXML
+	    private Label labelemailtaken;
+
+	    @FXML
+	    private Label labelpassword;
+
+	    @FXML
+	    private PasswordField textfieldpassword;
+
+	    @FXML
+	    private Label labelrepeatpassword;
+
+	    @FXML
+	    private PasswordField textfieldrepeatpassword;
+
+	    @FXML
+	    private Label labelcity;
+
+	    @FXML
+	    private TextField textfieldcity;
+
+	    @FXML
+	    private Label labelcountry;
+
+	    @FXML
+	    private TextField textfieldcountry;
+
+	    @FXML
+	    private Label labelcurrency;
+
+	    @FXML
+	    private ChoiceBox<?> choiceboxcurrency;
+	
+	
+
+
+}
