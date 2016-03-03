740ebf0ac535095b836093e575f31bfe8ae6b26f
kamster <kamster94@gmail.com>
2016-03-03 13:09:13 +0100

    Nowe metody w interfejsie dodawania użytkownika
    Signed-off-by: kamster <kamster94@gmail.com>

--- strider/src/dbConnection/CreateUser.java
+++ strider/src/dbConnection/CreateUser.java
@@ -9,16 +9,28 @@ public class CreateUser {
 	private String email;
 	private String country;
 	private String password;
-	private int currency; 
+	private String currency; 
 	
-	public CreateUser(String userName, String city, String email, String country, String password, int currency){
-		//TODO: sprawdzenie, czy dane s¹ poprawne
+	public CreateUser(String userName, String city, String email, String country, String password, String currency){
 		this.userName = userName;
 		this.city = city;
 		this.email = email;
 		this.country = country;
 		this.password = password;
 		this.currency = currency;
+	}
+	
+	public boolean verifyDataValidity(){
+		if (!email.contains("@")) return false;
+		if (currency.length()!=3) return false;
+		if (userName.length()>15) return false;
+		if (password.length()<5) return false;
+		else return true;
+	}
+	
+	public boolean checkEmailAvailability(){
+		DbAccess dbConnection = new DbAccess("DBA","sql");
+		return dbConnection.pushToDb("TODO: sql", Arrays.asList(email));
 	}
 	
 	public boolean sendToDb(){
