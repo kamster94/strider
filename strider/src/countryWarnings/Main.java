51bd1c6f3c2a1a661de11735b68bdf2dc54147ef
Artur <arturkielbiewski@gmail.com>
2016-03-08 21:22:23 +0100

    Dodana wyszukiwarka ciekawych miejsc w kraju oraz zamiana txt na html

--- strider/src/countryWarnings/Main.java
+++ strider/src/countryWarnings/Main.java
@@ -1,7 +1,12 @@
 package countryWarnings;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
@@ -14,7 +19,10 @@ import np.com.ngopal.control.AutoFillTextBox;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.fxml.FXMLLoader;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -24,21 +32,29 @@ import org.jsoup.select.Elements;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
+import org.apache.commons.lang3.StringUtils;
 
 public class Main extends Application {
 	
-	 public static final String url = "http://www.polakzagranica.msz.gov.pl";	   
+	 public static final String url = "http://www.polakzagranica.msz.gov.pl";	
+	 public static final String url2 = "https://pl.wikipedia.org";
 	 public static ArrayList<String> countryNames;
+	 public static ArrayList<String> countryNames2;
 	 public static ArrayList<String> countryHtmls;
+	 public static ArrayList<String> wikiCitiesList;
+	 public static ArrayList<String> wikiCitiesListHtmls;
 	 public static ComboBox box ;
+	 public static ComboBox box2 ;
 	 public static AutoCompleteComboBoxListener countryNameBox2;
-	 
+	 public static ObservableList data;
+	 public static ObservableList data2;
+	 public static WebView browser;
+	 public static WebEngine webEngine;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		Document document;
-		ObservableList data = FXCollections.observableArrayList();
+		data = FXCollections.observableArrayList();
 		 try {
 				//Ponizsze linijki sa do sciagniecia listy Krajow z mszu
 			    //Dostawanie sie do ostrzezen informacji odbywa sie w klasie SampleController.java
@@ -52,6 +68,7 @@ public class Main extends Application {
 
 				countryNames = new ArrayList();
 				countryHtmls = new ArrayList();
+				wikiCitiesListHtmls = new ArrayList();
 								
 				for (Element link : elems) {
 					countryNames.add(link.text());
@@ -63,7 +80,7 @@ public class Main extends Application {
 				rozmiar = rozmiar/2;
 				
 				//Z jakiegos powodu sciaga do obu list dwa razy to samo, mimo ze w htmlu
-				//sie nie powtarza. Tymczasowo na betonie po prostu usune polowe listy
+				//sie nie powta¿a. Tymczasowo na betonie po prostu usune polowe listy
 				for(int i = countryNames.size()-1; i >= rozmiar; i--){
 				countryNames.remove(i);
 				countryHtmls.remove(i);
@@ -76,14 +93,27 @@ public class Main extends Application {
 				for(int j=0; j<countryNames.size(); j++)
 				data.add(countryNames.get(j));
 				
-				
-				
+		
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			    
+		/*	    
+		 try {
+			 
+			 File file = new File("miasta.txt");
+			 
+			 try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+				    for(String line; (line = br.readLine()) != null; ) {
+				       wikiCitiesListHtmls.add(line.substring(line.lastIndexOf(" ")+1));
+				    }
+				}
+			 					
 		
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}*/
 
 		
 		try {		
@@ -95,9 +125,18 @@ public class Main extends Application {
 			box.setLayoutY(40);
 			box.setPrefWidth(190);
 			
+			box2 = new ComboBox();	
+			//box2.setItems(data);
+			new AutoCompleteComboBoxListener(box2);
+			box2.setLayoutX(490);
+			box2.setLayoutY(40);
+			box2.setPrefWidth(190);
+		
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			root.getChildren().add(box);
-			Scene scene = new Scene(root,590,520);
+			//root.getChildren().add(scrollPane);
+			//root.getChildren().add(box2);
+			Scene scene = new Scene(root,935,520);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -105,6 +144,10 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	
+	
 	
 	public static void main(String[] args) {
 	launch(args);
--- strider/src/countryWarnings/SampleController.java
+++ strider/src/countryWarnings/SampleController.java
@@ -1,15 +1,22 @@
 package countryWarnings;
 
 
+import org.json.*;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -17,11 +24,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 
 
 public class SampleController implements Initializable{
@@ -30,21 +42,31 @@ public class SampleController implements Initializable{
     private Button findButton;
 
     @FXML
-    private TextArea informationTextArea;
-
+    private Button findCityButton;
+    
+    @FXML
+    private TextField cityTextField;
+    
+    @FXML
+    private WebView cityWebView;
+    
+    @FXML
+    private WebView countryWebView;
+    
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
-		informationTextArea.setWrapText(true);
 
+		String newLine = System.getProperty("line.separator");
 		findButton.setOnAction(new EventHandler<ActionEvent>() {
 	        @Override
 	        public void handle(ActionEvent arg0) {
 	        	   
+	        		String htmlString = "";
+	        	    String finalmente = "<font face='WildWest'>";        	
 	        		String textFromCountryNameBox = Main.box.getEditor().getText(); 
 	        		String url = "http://www.polakzagranica.msz.gov.pl";	   
 	        	    Document document;
-	        	    String newLine = System.getProperty("line.separator");
+	        	    
 	        	    int checkForMatchingCountry = 0;  
 					int num=0;
 	        	    	        	  	
@@ -71,7 +93,6 @@ public class SampleController implements Initializable{
 					Elements elems3 = document.select(("div[class=warning]"));//ostrzezenia o panstwie
 					Elements elems4 = document.select(("div[class=item]")); //informacje o panstwie
 					
-					informationTextArea.setText(""); ////czyszczenie poprzednich wyszukan
 					
 					for (Element link : elems3) {
 						
@@ -82,67 +103,162 @@ public class SampleController implements Initializable{
 							if(elem.tagName().equals("a")){
 							
 							Elements linkGrandChildren = elem.children();
-							for (Element elem2 : linkGrandChildren) {
+								for (Element elem2 : linkGrandChildren) {
  
 								if(elem2.tagName().equals("h3"))
-							    informationTextArea.appendText(elem2.text().toUpperCase() + newLine);
-								
-							}
-			        
+								finalmente += "<b>" + elem2.text().toUpperCase() + "</b></br>";						
+								}	        
 							}
 					        if(elem.tagName().equals("div"))
-						    informationTextArea.appendText(elem.text() + newLine);
-					        
-					        
+					        finalmente += elem.text() + "</br>";				        
 					    }
-					
-				//	informationTextArea.appendText(link.text() + newLine); //odczytywanie tekstu ze zrodla do TextArea
-					
-						informationTextArea.appendText(newLine);
-								
-					
-					}
-					
-					
+						finalmente += "</br>";				
+	
+					}										
 					for (Element link : elems4) {
 						
 						Elements linkChildren = link.children();
 						
-						for (Element elem : linkChildren) {
-					        
+						for (Element elem : linkChildren) {					        
 					        if(elem.tagName().equals("h3"))
-					        informationTextArea.appendText(elem.text().toUpperCase() + newLine);
-					        if(elem.tagName().equals("div"))
-						    informationTextArea.appendText(elem.text() + newLine);
+					        finalmente += "<b>" + elem.text().toUpperCase() + "</b></br>";
 					        
+					        if(elem.tagName().equals("div"))
+						    finalmente += elem.text() + "</br>";
 					        
 					    }
-					
-				//	informationTextArea.appendText(link.text() + newLine); //odczytywanie tekstu ze zrodla do TextArea
-					
-						informationTextArea.appendText(newLine);
-								
+						finalmente += "</br></font>";								
 					}
 					
-					
-					
-					informationTextArea.selectPositionCaret(0); //przewiniecie do poczatku tekstu w TextArea
+					File htmlTemplateFile = new File("./htmls/template.html");
+	  				htmlString = FileUtils.readFileToString(htmlTemplateFile);
+	  				String title = "";
+	  				htmlString = htmlString.replace("$title", title);
+	  				htmlString = htmlString.replace("$body", finalmente);
+	  				File newHtmlFile = new File("./htmls/" + textFromCountryNameBox + ".html");
+	  				FileUtils.writeStringToFile(newHtmlFile, htmlString);
+
 						
-					}else
-					informationTextArea.setText("Prosze wpisac poprawnie nazwe Panstwa");
-				
+					}else{
+					
+					finalmente ="<b>Prosze wpisac poprawnie nazwe Panstwa<b>";
+
+					}
+					
+					countryWebView.getEngine().loadContent(finalmente);
+					
+					/*
+					String url2 = Main.url2;
+					url2+= Main.wikiCitiesListHtmls.get(num);
+					Main.data2 = FXCollections.observableArrayList();
+					
+					document = Jsoup.connect(url2)
+					.userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+					.referrer("http://www.google.com")		
+					.get();
+					
+					Elements elems = document.select(("table[class=wikitable sortable]"));//ostrzezenia o panstwie
+
+					Element first = elems.first();
+					
+					Elements linkChildren = first.children();
+					
+					for (Element link : linkChildren) {
+						
+						
+						 
+						Elements linkGrandChildren = link.children();
+												
+							for (Element elem2 : linkGrandChildren) {					        
+								
+								Elements linkGrandGrandChildren = elem2.children();
+							
+								for (Element elem3 : linkGrandGrandChildren) {					        
+								     
+									Elements linkGrandGrandGrandChildren = elem3.children();
+									
+									for (Element elem5 : linkGrandGrandGrandChildren) {					         
+												if(elem5.tagName().equals("a"))
+											    Main.data2.add(elem5.text());
+											
+										    }
+						
+							}					        					        
+					    }																	
+					}
+					
+					Main.box2.setItems(Main.data2);
+					
+					*/
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 
-	              
+            
 	        }
 	    });
 		
-	
+		
+		findCityButton.setOnAction(new EventHandler<ActionEvent>() {
+	        @Override
+	        public void handle(ActionEvent arg0) {
+	        	String htmlString = ""; 
+	        	String textFromCityNameBox = cityTextField.getText(); 
+        		String url = "https://pl.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=10000&gspage=";
+        		String url2 =  "&gslimit=100&gsprop=type|name|&format=json";	   
+        		url += textFromCityNameBox;
+        		url += url2;
+        
+        		String finalmente = "";
+        	    try{
+        		  InputStream is = new URL(url).openStream();
+        	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+        	      String jsonText = readAll(rd);
+        	      JSONObject json = new JSONObject(jsonText);
+        	      System.out.println(jsonText);
+
+        	      JSONArray arr  = json.getJSONObject("query").getJSONArray("geosearch");
+				  
+				  finalmente += "<b><font face='WildWest'>INTERESUJ¥CE MIEJSCA I WYDARZENIA</b></font></br></br>";
+  				for (int i = 0; i < arr.length(); i++)
+  				{
+  					String title = arr.getJSONObject(i).getString("title");  				   		
+  				    finalmente += title;
+  				    finalmente += "</br>";  	  				    
+  				}
+  			//	System.out.println(finalmente); 
+  				finalmente += "</font>";
+  				File htmlTemplateFile = new File("./htmls/template.html");
+  				htmlString = FileUtils.readFileToString(htmlTemplateFile);
+  				String title = "";
+  				htmlString = htmlString.replace("$title", title);
+  				htmlString = htmlString.replace("$body", finalmente);
+  				File newHtmlFile = new File("./htmls/" + textFromCityNameBox + ".html");
+  				FileUtils.writeStringToFile(newHtmlFile, htmlString);
+      	 
+  			//	Main.webEngine.load("new.html");
+  			//	cityWebView.getEngine().load(newHtmlFile.toURI().toURL().toString());  				
+  					 				
+        	    }catch(Exception e){
+        	    	 cityWebView.getEngine().loadContent("<b>Proszê poprawnie wpisaæ nazwê miasta<b>");
+        	        	    }
+       	   
+        	    //cityWebView.getEngine().load("new.html");  	
+        	    cityWebView.getEngine().loadContent(finalmente);
+        	     
+	        	
+	        }
+	    });
 
     
 	}
-    
+	 private static String readAll(Reader rd) throws IOException {
+	    StringBuilder sb = new StringBuilder();
+	    int cp;
+	    while ((cp = rd.read()) != -1) {
+	      sb.append((char) cp);
+	    }
+	    return sb.toString();
+	  }
 }
