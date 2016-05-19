615969beda504756612ee432fdd7c3bc4c2182f4
kamster <kamster94@gmail.com>
2016-05-19 18:34:38 +0200

    Małe poprawki, niegitowanie htmlów

--- strider/src/dbConnection/Test.java
+++ strider/src/dbConnection/Test.java
@@ -1,13 +1,9 @@
 package dbConnection;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Test {
 
 	public static void main(String[] args) {
-		DbAccess test = new DbAccess("Kamster","sql");
+		DbAccess test = DbAccess.getInstance();
 		int create = test.getIntFromDb("CALL DBA.fAddToUsers (@email = 'kam@test.pl',"
 				+ " @haslo = 'trudnehaslo', @username = 'testkamster', @idcurrency = 1, @idcountry = 1, @idcity = 1)");
 		System.out.println(create);
