6f64894d0b336d97d5522b7b21812710d2a3cab9
kamster <kamster94@gmail.com>
2016-05-20 10:24:46 +0200

    Poprawka dla handlera logowania

--- strider/src/dbHandlers/DatabaseHandlerLogin.java
+++ strider/src/dbHandlers/DatabaseHandlerLogin.java
@@ -5,24 +5,30 @@ import dbConnection.DbAccess;
 
 public class DatabaseHandlerLogin {
 	
-	public static DbAccess dataBaseAccess;
+	public static DbAccess dbConnection;
 	public User user;
 	
 	public DatabaseHandlerLogin(){
-		dataBaseAccess = DbAccess.getInstance();
+		dbConnection = DbAccess.getInstance();
 	}
 	
 	
 	public int loginUser(String email, String password){
-		int status = dataBaseAccess.getIntFromDb("SELECT DBA.fCheckUser('" + email + "', '" + password + "')");
+		int status = dbConnection.getIntFromDb("SELECT DBA.fCheckUser('" + email + "', '" + password + "')");
 		
 		if (status == 0) {
 			user = User.getInstance();
 			user.setEmail(email);
-			String userName = DbAccess.getInstance().getSingeStringFromDb("SELECT UserName FROM DBA.UserData WHERE UserData.Email = '" + email + "'", "UserName");
-			int userId = DbAccess.getInstance().getIntFromDb("SELECT IDUser FROM DBA.UserData WHERE UserData.Email = '" + email + "'");
+			String userName = dbConnection.getSingeStringFromDb("SELECT UserName FROM DBA.UserData WHERE UserData.Email = '" + email + "'", "UserName");
+			int userId = dbConnection.getIntFromDb("SELECT IDUser FROM DBA.UserData WHERE UserData.Email = '" + email + "'");
+			int currencyId = dbConnection.getIntFromDb("SELECT IDCurrency FROM DBA.UserData WHERE UserData.Email = '" + email + "'");;
+			int cityId = dbConnection.getIntFromDb("SELECT IDCity FROM DBA.UserData WHERE UserData.Email = '" + email + "'");;
+			int countryId = dbConnection.getIntFromDb("SELECT IDCountry FROM DBA.UserData WHERE UserData.Email = '" + email + "'");;
 			user.setId(userId);
 			user.setUserName(userName);
+			user.setCurrencyId(currencyId);
+			user.setCityId(cityId);
+			user.setCountryId(countryId);
 		}
 		return status;
 	}
