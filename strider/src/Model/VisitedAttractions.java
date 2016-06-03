4907702b399d24a2979552c1610523e3187e96c1
kamster <kamster94@gmail.com>
2016-06-03 23:08:10 +0200

    Pełen komplet informacji dla odwiedzonych

--- strider/src/Model/VisitedAttractions.java
+++ strider/src/Model/VisitedAttractions.java
@@ -8,6 +8,11 @@ public class VisitedAttractions {
 	private int cityId;
 	private int attractionId;
 	private String attractionName;
+	private String streetName;
+	private String streetNumber;
+	private String zipCode;
+	private String openingTime;
+	private String closingTime;
 	
 	private DbAccess dbConnection;
 	
@@ -17,6 +22,11 @@ public class VisitedAttractions {
 		this.cityId = cityId;
 		this.attractionId = attractionId;
 		this.attractionName = dbConnection.getSingeStringFromDb("SELECT AttractionName FROM DBA.Attraction WHERE IDCountry = " + countryId + " AND IDCity = " + cityId + " AND IDAttraction = " + attractionId, "AttractionName");
+		this.streetName = dbConnection.getSingeStringFromDb("SELECT StreetName FROM DBA.Attraction WHERE IDCountry = " + countryId + " AND IDCity = " + cityId + " AND IDAttraction = " + attractionId, "StreetName");
+		this.streetNumber = dbConnection.getSingeStringFromDb("SELECT StreetNumber FROM DBA.Attraction WHERE IDCountry = " + countryId + " AND IDCity = " + cityId + " AND IDAttraction = " + attractionId, "StreetNumber");
+		this.zipCode = dbConnection.getSingeStringFromDb("SELECT ZipCode FROM DBA.Attraction WHERE IDCountry = " + countryId + " AND IDCity = " + cityId + " AND IDAttraction = " + attractionId, "ZipCode");
+		this.openingTime = dbConnection.getSingeStringFromDb("SELECT OpeningTime FROM DBA.Attraction WHERE IDCountry = " + countryId + " AND IDCity = " + cityId + " AND IDAttraction = " + attractionId, "OpeningTime");
+		this.closingTime = dbConnection.getSingeStringFromDb("SELECT ClosingTime FROM DBA.Attraction WHERE IDCountry = " + countryId + " AND IDCity = " + cityId + " AND IDAttraction = " + attractionId, "ClosingTime");
 	}
 	
 	public int getCountryId() {
@@ -35,4 +45,29 @@ public class VisitedAttractions {
 		return attractionName;
 	}
 
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public String getStreetNumber() {
+		return streetNumber;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public String getOpeningTime() {
+		return openingTime;
+	}
+
+	public String getClosingTime() {
+		return closingTime;
+	}
+
+	public DbAccess getDbConnection() {
+		return dbConnection;
+	}
+
+	
 }
